b8b8ba065b1be0fc4f4c47e8505d9893d793326c
timothylim <timothy_lim@brown.edu>
2015-04-23 18:22:25 -0400

    Assignment-4

--- 3jsbot_forward_kinematics.js
+++ 3jsbot_forward_kinematics.js
@@ -17,11 +17,95 @@ traverse_forward_kinematics_link
 traverse_forward_kinematics_joint
 compute_and_draw_heading
 */
- function robot_forward_kinematics () {
+function robot_forward_kinematics () {
  	
  	mstack = generate_identity();
  	traverse_base_link("link1", mstack);
-    
-
  }
 
+function transform_joint(joint,mstack) {
+	
+	var x = robot.joints[joint].origin.xyz[0];
+    var y = robot.joints[joint].origin.xyz[1];
+    var z = robot.joints[joint].origin.xyz[2];
+    var r = robot.joints[joint].origin.rpy[0];
+    var p = robot.joints[joint].origin.rpy[1];
+    var rot_y = robot.joints[joint].origin.rpy[2];
+
+    var result = multiplyMatrices(mstack,generate_matrix_transform(x,y,z,r,p,rot_y));
+    var tempmat = matrix_2Darray_to_threejs(result);
+
+    simpleApplyMatrix(robot.joints[joint].geom,tempmat);
+
+    return result;
+}
+
+function transform_link(link,mstack) {
+	
+	if (link == "link1") {
+
+		var link = robot.base;
+		var x = robot.origin.xyz[0];
+	    var y = robot.origin.xyz[1];
+	    var z = robot.origin.xyz[2];
+	    var r = robot.origin.rpy[0];
+	    var p = robot.origin.rpy[1];
+	    var rot_y = robot.origin.rpy[2];
+
+	    result = multiplyMatrices(mstack,generate_matrix_transform(x,y,z,r,p,rot_y));
+	    var tempmat = matrix_2Darray_to_threejs(result);
+	    
+	    simpleApplyMatrix(robot.links[link].geom,tempmat);
+   		
+   		return result;
+	}
+
+	var joint = robot.links[link].parent_joint;
+	var x = robot.joints[joint].origin.xyz[0];
+    var y = robot.joints[joint].origin.xyz[1];
+    var z = robot.joints[joint].origin.xyz[2];
+    var r = robot.joints[joint].origin.rpy[0];
+    var p = robot.joints[joint].origin.rpy[1];
+    var rot_y = robot.joints[joint].origin.rpy[2];
+
+    result = multiplyMatrices(mstack,generate_matrix_transform(x,y,z,r,p,rot_y));
+    var tempmat = matrix_2Darray_to_threejs(result);
+    
+    simpleApplyMatrix(robot.links[link].geom,tempmat);
+
+    return result;
+}
+
+
+function traverse_base_link(link,mstack) {
+
+	mstack = transform_link(link,mstack);
+	limit = robot.links[link].children.length;
+	
+	for (i = (0); i < limit; i++) { 
+		joint = robot.links[link].children[i];
+		
+		if (joint != undefined) {
+			traverse_joint(joint, mstack);
+		}
+	}
+
+}
+
+function traverse_child_links(link, mstack) {
+
+	mstack = transform_link(link,mstack);
+	joint = robot.links[link].children[0];
+	
+	if (joint != undefined) {
+		traverse_joint(joint, mstack);
+	}
+}
+
+
+function traverse_joint(joint,mstack) {
+
+	transform_joint(joint,mstack);
+	var link = robot.joints[joint].child;
+	traverse_child_links(link, mstack)
+}
